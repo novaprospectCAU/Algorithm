dd522f22bf3afff88c52cce012f66e03c33b9bfe
novaprospectCAU <novaprospect110a@gmail.com>
2024-07-10 16:18:50 +0900

    선택 정렬 알고리즘을 다른 함수에서 수행하는 것으로 수정했습니다

--- algs/sorts/selection/main.cpp
+++ algs/sorts/selection/main.cpp
@@ -1,11 +1,9 @@
 // 선택 정렬 : 가장 작은 값을 계속 앞으로 보내는 정렬
 #include <iostream>
 
-// 편의상 양수 signed int범위 내의 값만을 받는다고 가정합니다.
-int main()
+// 편의상 signed int범위 내의 값만을 받는다고 가정합니다.
+void selection_sort(int arr[], int size)
 {
-  int size = 10;
-  int arr[] = {8, 5, 2, 6, 9, 3, 0, 4, 1, 7};
   int min = arr[0];
   int index = 0;
   for (int y = 0; y < size; y++)
@@ -24,6 +22,13 @@ int main()
     arr[y] = arr[index];
     arr[index] = temp;
   }
+}
+
+int main()
+{
+  int size = 10;
+  int arr[] = {8, 5, 2, 6, 9, 3, 0, 4, 1, 7};
+  selection_sort(arr, sizeof(arr) / sizeof(int));
   for (int i = 0; i < size; i++)
   {
     std::cout << arr[i] << ' ';
